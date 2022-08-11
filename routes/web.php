479a65a2f24424de4d7be77ad57c0d79bbcfd8be
Imran Ahmad <meetimrang@gmail.com>
2022-08-11 12:47:02 +0500

    Paypal integration into Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DateController;
+use App\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/date', [DateController::class, 'date']);
+Route::post('/paypal', [PaymentController::class, 'pay'])->name('payment');
+Route::get('success', [PaymentController::class, 'success']);
+Route::get('error', [PaymentController::class, 'error']);
+Route::view('/pay','paypal');
